e450282a9fccc5190ce69fad32fe3d6b05d4b095
Michael Odongo <mikodongo@gmail.com>
2020-05-09 07:10:46 +0300

    Updates

---
+++ precache-manifest.85c816f365e94a9a8c6ffaf692f91dd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2456d0e46575a20f1b2f8d7234dadcc9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "77a1ae198f316b9c56fc",
+    "url": "/monsters-rolodex/static/css/main.678ba037.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77a1ae198f316b9c56fc",
+    "url": "/monsters-rolodex/static/js/main.d17f64d4.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
